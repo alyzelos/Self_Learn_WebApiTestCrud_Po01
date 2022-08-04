dc482b7aca4909a0a291b85007aab9621b97f38c
pojarcosminalin <pojarcosminalin@gmail.com>
2022-08-04 16:30:38 +0300

    2022 08 04

---
+++ WebApiTestCrud_Po01/StudentException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace WebApiTestCrud_Po01
+{
+    [Serializable]
+    internal class StudentException : Exception
+    {
+        public StudentException()
+        {
+        }
+
+        public StudentException(string message) : base(message)
+        {
+        }
+
+        public StudentException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected StudentException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
