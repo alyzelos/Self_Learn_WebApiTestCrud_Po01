7c06c5cfbf6a70bab3297a4a962103a7c6e82c6e
alyzelos <pojarcosminalin@gmail.com>
2022-09-13 17:59:44 +0300

    20220913 last commit

--- WebApiTestCrud_Po05/Infrastructure/EmployeeContext.cs
+++ WebApiTestCrud_Po05/Infrastructure/EmployeeContext.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace WebApiTestCrud_Po05.Infrastructure
 {
-    public class EmployeeContext
+    public class EmployeeContext : DbContext
+
     {
         
     }
