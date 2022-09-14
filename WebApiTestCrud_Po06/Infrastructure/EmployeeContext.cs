5ac375c8e6880e6a9d3366d0f9fd24e218c76438
alyzelos <pojarcosminalin@gmail.com>
2022-09-14 16:12:37 +0300

    20220914 - web api proj 7 created for c# language version

---
+++ WebApiTestCrud_Po06/Infrastructure/EmployeeContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Debug;
+
+namespace WebApiTestCrud_Po06.Infrastructure
+{
+    public class EmployeeContext : DbContext
+    {
+        private static readonly LoggerFactory _myLoggerFactory = new(new[] { new DebugLoggerProvider() });
+
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        { }
+
+        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLoggerFactory(_myLoggerFactory);
+        }
+    }
+}
