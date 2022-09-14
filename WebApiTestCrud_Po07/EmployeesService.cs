8f5d92051e4d360b3e69bec2c5ee159ba448f3fe
alyzelos <pojarcosminalin@gmail.com>
2022-09-14 16:38:42 +0300

    20220914

---
+++ WebApiTestCrud_Po07/EmployeesService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiTestCrud_Po07.Infrastructure;
+using WebApiTestCrud_Po07.Infrastructure.Model;
+
+namespace WebApiTestCrud_Po07
+{
+    public class EmployeesService : IEmployeesService
+    {
+        private readonly EmployeeContext _employeeContext;
+        public EmployeesService(EmployeeContext employeeContext)
+        {
+            _employeeContext = employeeContext;
+        }
+
+        public IEnumerable<Employee> GetEmployeesFromDb()
+        {
+            var employeeFromDb = _employeeContext.Employees.ToList();
+            return employeeFromDb;
+        }
+    }
+}
---
+++ WebApiTestCrud_Po07/Infrastructure/EmployeeContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Debug;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiTestCrud_Po07.Infrastructure.Model;
+
+namespace WebApiTestCrud_Po07.Infrastructure
+{
+    public class EmployeeContext : DbContext
+    {
+
+        public static readonly LoggerFactory _myLoggerFactory = new(new[] { new DebugLoggerProvider() });
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLoggerFactory(_myLoggerFactory);
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
