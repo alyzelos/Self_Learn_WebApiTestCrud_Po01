21df272ac45f14f1688b5987ad4db571b0bde67b
alyzelos <pojarcosminalin@gmail.com>
2022-09-14 20:10:04 +0300

    20220914 last office save

---
+++ WebApiTestCrud_Po07/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiTestCrud_Po07.Controllers
+{
+    [Route("{controller}")]
+    [ApiController]
+    public class EmployeesController : Controller
+    {
+        private readonly IEmployeesService _employeesService;
+
+        public EmployeesController(IEmployeesService employeesService)
+        {
+            _employeesService = employeesService;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var employeesFromDb = _employeesService.GetEmployeesFromDb();
+            return Ok(employeesFromDb);
+        }
+    }
+}
--- WebApiTestCrud_Po07/Startup.cs
+++ WebApiTestCrud_Po07/Startup.cs
@@ -1,13 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
+using WebApiTestCrud_Po07.Infrastructure;
 
 namespace WebApiTestCrud_Po07
 {
@@ -23,7 +22,25 @@ namespace WebApiTestCrud_Po07
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+           
+            //injecting DBcontext configuration string
+            services.AddDbContext<EmployeeContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("Default"));
+            });
+            //Adding configuration for not sending null values to JSON
+            services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
+            });
+            services.AddSwaggerGen(c => 
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "webApiTestPo07", Version = "v1" });
+            });
+
+            services.AddScoped<IEmployeesService, EmployeesService>();
+            //adding swagger to app
+            //services.AddSwaggerGen
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,24 +49,17 @@ namespace WebApiTestCrud_Po07
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "webApiTestPo07 v1"));
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
             app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
