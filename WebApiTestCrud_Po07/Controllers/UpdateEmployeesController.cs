79f32be2395b893a678e7e49bb2f68a9f2dc89e5
alyzelos <pojarcosminalin@gmail.com>
2022-09-20 15:39:51 +0300

    2020920 02

---
+++ WebApiTestCrud_Po07/Controllers/UpdateEmployeesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiTestCrud_Po06.Infrastructure.Model;
+
+namespace WebApiTestCrud_Po07.Controllers
+{
+        [Route("{controller}")]
+        [ApiController]
+    public class UpdateEmployeesController
+    {
+        private readonly IEmployeesService _employeesService;
+
+        public UpdateEmployeesController(IEmployeesService employeesService)
+        {
+            _employeesService = employeesService;
+        }
+
+        [HttpPut]
+        public IActionResult Put(int id, [FromBody] Employee employee)
+
+    }
+}
