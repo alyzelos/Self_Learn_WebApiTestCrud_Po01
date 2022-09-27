e71ea7ce72488089d7ee180132f2e7d8c7b70a34
alyzelos <pojarcosminalin@gmail.com>
2022-09-27 22:45:08 +0300

    20220927 -> last commit from dinj video

---
+++ DependecyInjectionFullyExplain/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DependecyInjectionFullyExplain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new HelloService();
+            var consumer = new ServiceConsumer(service);
+            service.Print();
+            consumer.Print();
+        }
+        public class ServiceConsumer
+        {
+            private readonly HelloService hello;
+
+            public ServiceConsumer(HelloService hello)
+            {
+                this.hello = hello;
+            }
+            public void Print()
+            {
+                this.hello.Print();
+            }
+        }
+        public class HelloService
+        {
+            public void Print()
+            {
+                Console.WriteLine("hello word");
+            }
+        }
+    }
+
+}
--- WebApiTestCrud_Po01/DTOs/GradeDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApiTestCrud_Po01.Controllers.DTOs
-{
-    public class GradeDto
-    {
-        public int GradeID { get; set; }
-        public string GradeName { get; set; }
-        public string Section { get; set; }
-    }
-}
