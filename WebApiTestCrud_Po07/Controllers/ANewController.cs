5c2e073ffcc9a73af695b5aaea3ededc172eaee1
Cosmin Pojar <cosmin.pojar@t-2.srl>
2022-11-02 11:25:05 +0200

    20221102

---
+++ WebApiTestCrud_Po07/Controllers/ANewController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiTestCrud_Po07.Controllers
+{
+    public class ANewController
+    {
+    }
+}
